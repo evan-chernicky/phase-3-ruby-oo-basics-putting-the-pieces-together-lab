d6d9439414ae1784fabb7bde7b1010b80cd2a94a
Evan Chernicky <echernicky@gmail.com>
2022-03-15 19:23:56 -0400

    commit tree 14042a75bf706226a4ce0dd71343accc31d902be

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = "And Then There Were None"
+    end
+
+    def turn_page()
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+harry_potter = Book.new("Harry Potter")
+harry_potter.title()
